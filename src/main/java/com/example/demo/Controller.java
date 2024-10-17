829432156dd6f41d1a9da9fafa4c5d6b46a28d2a
shashikumar9604 <97779746+shashikumar9604@users.noreply.github.com>
2024-10-17 09:55:21 +0530

    Update Controller.java

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -8,7 +8,7 @@ public class Controller {
 	
 	@GetMapping("/welcome")
 	public String callMethosApi() {
-		return "Welcome to Java Learning shashi updated for webhook test";
+		return "Welcome to Java Learning shashi updated for webhook test demo";
 	}
 
 }
